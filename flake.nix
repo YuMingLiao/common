3e4706a59b030b7af4e4d60c1473534f4f0e9951
YuMingLiao <dycan0716@gmail.com>
2024-10-10 03:04:01 +0000

    rename to common; add common flake libs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,36 @@
 {
-  description = "mylib flake";
+  description = "my common flake";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    nixpkgs-24_05.url = "github:nixos/nixpkgs/nixos-24.05"; # Used for my nixos
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }: {
-    overlay.default = import ./haskell-lib.nix;
-    defaultPackages.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.hello;
-  };
+  outputs =
+    inputs@{ self, nixpkgs, ...}:
+    let
+      overlayed-nixpkgs = nixpkgs.legacyPackages.x86_64-linux.appendOverlays [self.overlay.default];
+    in
+    {
+      overlay.default = import ./haskell-lib.nix;
+      #defaultPackages.x86_64-linux = overlayed-nixpkgs;
+      legacyPackages.x86_64-linux = overlayed-nixpkgs;
+      lib.mkFlake =
+        args: mod:
+        inputs.flake-parts.lib.mkFlake
+          {
+            inputs = args.inputs // {
+              inherit (inputs) nixpkgs;
+            };
+          }
+          {
+            systems = import inputs.systems;
+            imports = [
+              inputs.haskell-flake.flakeModule
+            ];
+          };
+    };
 }
