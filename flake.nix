1d522b8e1e5d2e2393c6e5644dd83d098fda902d
YuMingLiao <dycan0716@gmail.com>
2024-10-10 04:10:53 +0000

    disuse legacyPackages; add nipxkgs in mkFlake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    nixpkgs-24_05.url = "github:nixos/nixpkgs/nixos-24.05"; # Used for my nixos
+    #nixpkgs-24_05.url = "github:nixos/nixpkgs/nixos-24.05"; # Used for my nixos
     systems.url = "github:nix-systems/default";
   };
 
@@ -17,13 +17,12 @@
     {
       overlay.default = import ./haskell-lib.nix;
       #defaultPackages.x86_64-linux = overlayed-nixpkgs;
-      legacyPackages.x86_64-linux = overlayed-nixpkgs;
       lib.mkFlake =
         args: mod:
         inputs.flake-parts.lib.mkFlake
           {
             inputs = args.inputs // {
-              inherit (inputs) nixpkgs;
+              nixpkgs = overlayed-nixpkgs;
             };
           }
           {
