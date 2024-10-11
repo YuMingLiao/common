dd9376046a0bdacd47744f6468647a78ed9a5b75
YuMingLiao <dycan0716@gmail.com>
2024-10-11 04:25:35 +0000

    add check-flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    check-flake.url = "github:srid/check-flake";
     #nixpkgs-24_05.url = "github:nixos/nixpkgs/nixos-24.05"; # Used for my nixos
     systems.url = "github:nix-systems/default";
     snowfall-lib = {
