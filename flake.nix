52bf9e36af1c8030c1c3bfebc9405074b290513d
YuMingLiao <dycan0716@gmail.com>
2024-10-11 05:20:59 +0000

    add back nix-system

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     haskell-flake.url = "github:srid/haskell-flake";
     check-flake.url = "github:srid/check-flake";
     #nixpkgs-24_05.url = "github:nixos/nixpkgs/nixos-24.05"; # Used for my nixos
-    #systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/default";
     #snowfall-lib = {
     #  url = "github:snowfallorg/lib";
     #  inputs.nixpkgs.follows = "nixpkgs";
