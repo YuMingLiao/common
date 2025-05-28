c58d63c15a7f6bb2f67fc4bd3c56f9515de3fe01
YuMingLiao <dycan0716@gmail.com>
2025-05-28 03:15:05 +0000

    eliminate IFD

--- flake-module.nix
+++ flake-module.nix
@@ -4,8 +4,6 @@ common:
 {
   imports = [
     common.inputs.haskell-flake.flakeModule
-    ./nix/ghc9101.nix
-    ./nix/ghc966.nix
     ./nix/ghc965.nix
   ];
   perSystem = { self', system, inputs', lib, ... }: {
