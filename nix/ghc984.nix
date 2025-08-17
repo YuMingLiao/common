36a418c9d25d31dda03f373624e88721ddf7fa25
... <...>
2025-08-17 02:33:27 +0000

    add ghc984

---
+++ nix/ghc984.nix
@@ -0,0 +1,30 @@
+# To use this package set in your `haskell-flake` projects, set the
+# `basePackages` option as follows:
+#
+# > basePackages = config.haskellProjects.ghc9101.outputs.finalPackages;
+#
+{
+  perSystem =
+    {
+      pkgs,
+      lib,
+      config,
+      system,
+      inputs',
+      ...
+    }:
+    {
+
+      haskellProjects.ghc984 = {
+        projectFlakeName = "YuMingLiao:common";
+
+        # This is not a local project, so disable those options.
+        defaults.packages = { };
+        devShell.enable = false;
+        autoWire = [ ];
+
+        basePackages = pkgs.haskell.packages.ghc984;
+      };
+    };
+
+}
